808ee4ca387609cdd01dc0661eb17bc4001fe5c9
Ouassim <ProfessionnelleCompte00@gmail.com>
2024-03-30 02:42:21 +0100

    init chakra-ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import { Box, Text } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <p className='text-5xl text-red-800'>Hello World</p>
-    </div>
+    <Box>
+      <Text fontSize="20px" textAlign="center">Hello World</Text>
+    </Box>
   )
 }
 
