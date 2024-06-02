6937b0949d80bc14de873207bc6c6586332f19d4
Ouassim <ProfessionnelleCompte00@gmail.com>
2024-06-03 00:22:21 +0100

    adding vercel.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import {ChakraProvider} from "@chakra-ui/react"
 import './index.css'
 import {Provider} from "react-redux"
 import Strore from "./redux/store.jsx"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Strore}>
